3b7257a74de831b456e8e57b6ab74c6176e2aebe
Major Hayden <major@redhat.com>
2020-03-17 12:18:45 -0500

    Initial EC2 deployment work

---
+++ deploy_ec2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Get the current user.
+CURRENT_USER=$(whoami)
+
+# Create a temporary directory.
+TEMPDIR="/tmp/osbuild.personal.${CURRENT_USER}"
+mkdir -p $TEMPDIR
+
+# Deploy the EC2 infrastructure.
+ansible-playbook -i localhost, ec2/deploy_on_ec2.yml
+
+# Deploy osbuild and osbuild-composer.
+ansible-playbook -i ${TEMPDIR}/hosts.ini playbook.yml
