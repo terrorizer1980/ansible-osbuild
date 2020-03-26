75796bb4cf0f13e2dc47212a6fdf06655e2896cf
Major Hayden <major@redhat.com>
2020-03-26 11:40:12 -0500

    Ensure junit_xml python module is installed

--- jenkins/run_tests.sh
+++ jenkins/run_tests.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 export JUNIT_OUTPUT_DIR=junit/
 
-dnf -y install ansible
+dnf -y install ansible python3-junit_xml
 
 # Run a deployment
 echo -e "[test_instances]\nlocalhost ansible_connection=local" > hosts.ini
