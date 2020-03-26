50520e46529e9dd8ceb1db22ad6223bbe1d7c44c
Major Hayden <major@redhat.com>
2020-03-26 10:52:37 -0500

    Enable lint checks
    Skipping rule 403 because it's silly.
    
    Signed-off-by: Major Hayden <major@redhat.com>

--- jenkins/run_tests.sh
+++ jenkins/run_tests.sh
@@ -3,4 +3,7 @@ set -euxo pipefail
 
 # Run ansible-lint
 dnf -y install python3-ansible-lint
-# ansible-lint playbook.yml
+ansible-lint --version
+
+# Rule 403: Package installs should not use latest
+ansible-lint --force-color -x 403 playbook.yml 2>&1 | tee jenkins.log
