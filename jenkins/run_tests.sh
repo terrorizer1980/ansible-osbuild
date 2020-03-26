7f67b1d0f1297145cd6d2fabcb80a39e3edcc2a5
Major Hayden <major@redhat.com>
2020-03-26 11:38:33 -0500

    Skip linting for now

--- jenkins/run_tests.sh
+++ jenkins/run_tests.sh
@@ -3,10 +3,7 @@ set -euxo pipefail
 
 export JUNIT_OUTPUT_DIR=junit/
 
-dnf -y install ansible python3-ansible-lint
-
-# Rule 403: Package installs should not use latest
-ansible-lint --force-color -x 403 playbook.yml 2>&1 | tee jenkins.log
+dnf -y install ansible
 
 # Run a deployment
 echo -e "[test_instances]\nlocalhost ansible_connection=local" > hosts.ini
