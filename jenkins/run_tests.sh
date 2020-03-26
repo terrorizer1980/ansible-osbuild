7b4ac85bbe5440105e3bbcfa512d996be5128afd
Major Hayden <major@redhat.com>
2020-03-26 11:27:45 -0500

    🛠️ Attempt a full deployment now
    Signed-off-by: Major Hayden <major@redhat.com>

--- jenkins/run_tests.sh
+++ jenkins/run_tests.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -euxo pipefail
 
-# Run ansible-lint
-dnf -y install python3-ansible-lint
-ansible-lint --version
+dnf -y install ansible python3-ansible-lint
 
 # Rule 403: Package installs should not use latest
 ansible-lint --force-color -x 403 playbook.yml 2>&1 | tee jenkins.log
+
+# Run a deployment
+echo -e "[test_instances]\nlocalhost ansible_connection=local" > hosts.ini
+ansible-playbook -i hosts.ini playbook.yml
