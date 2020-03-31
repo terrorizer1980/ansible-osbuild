52dc3321e3c234e7ec34ec6bf3c5546de9a7a4a2
Major Hayden <major@redhat.com>
2020-03-31 12:53:47 -0500

    Run integration tests optionally
    Set `run_integration_tests` to `yes` to run integration tests.
    
    Signed-off-by: Major Hayden <major@redhat.com>

--- jenkins/run_tests.sh
+++ jenkins/run_tests.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 set -euxo pipefail
 
-uname -a
-
+# Set an output path for the junit file.
 export JUNIT_OUTPUT_DIR=junit/
 
+# Ensure we have the right packages installed.
 sudo dnf -y install ansible python3-junit_xml
+
+# Check to see if we are supposed to run integration tests.
+if [[ ${RUN_INTEGRATION_TESTS:-0} == 1 ]]; then
+  EXTRA_VARS="-e 'run_integration_tests=yes'"
+fi
 
 # Run a deployment
 echo -e "[test_instances]\nlocalhost ansible_connection=local" > hosts.ini
-ansible-playbook -i hosts.ini playbook.yml
+ansible-playbook -i hosts.ini ${EXTRA_VARS:-} playbook.yml
 
-# Get the integration test logs.
-# cp /tmp/composer_tests.log.xz $WORKSPACE
+# Get the integration test logs if they exist and we are running under Jenkins.
+if [ -e "/tmp/composer_tests.log.xz" ] && [ -n "${WORKSPACE:-}"] ; then
+  cp /tmp/composer_tests.log.xz $WORKSPACE
+fi
