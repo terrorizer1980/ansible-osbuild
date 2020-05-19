7e110d56f394136cd48b03530e4e4fdcbd833591
Major Hayden <major@redhat.com>
2020-05-19 11:25:50 -0500

    Run a quick composer test after deployment
    The CI for ansible-osbuild would be a lot more solid if we tried at
    least one compose at the end. 😉
    
    Signed-off-by: Major Hayden <major@redhat.com>

--- schutzbot/run_tests.sh
+++ schutzbot/run_tests.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 set -euxo pipefail
 
-# Get OS information.
-source /etc/os-release
-
 # Create temporary directories for Ansible.
 sudo mkdir -vp /opt/ansible_{local,remote}
 sudo chmod -R 777 /opt/ansible_{local,remote}
 
-# Build dnf cache.
-dnf makecache
-
 # Run deployment.
 echo -e "[test_instances]\nlocalhost ansible_connection=local" > hosts.ini
 ansible-playbook -i hosts.ini ${EXTRA_VARS:-} playbook.yml
+
+# Mount a ramdisk on /run/osbuild to speed up testing.
+sudo mkdir -p /run/osbuild
+sudo mount -t tmpfs tmpfs /run/osbuild
+
+# Once everything is deployed, add a blueprint and build it just to ensure
+# everything is working.
+ansible-playbook -i hosts.ini schutzbot/composer-smoke-test.yml
