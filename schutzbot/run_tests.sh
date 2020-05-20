7c74a86f003a74af0f5c63fe1d2cade05c0de5e3
Major Hayden <major@redhat.com>
2020-05-20 14:30:08 -0500

    Use known working package for CI
    Signed-off-by: Major Hayden <major@redhat.com>

--- schutzbot/run_tests.sh
+++ schutzbot/run_tests.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 set -euxo pipefail
 
+# Get OS details.
+source /etc/os-release
+
+# Set up a dnf repository for a previously built known working package of
+# osbuild and osbuild-composer.
+REPO_BASE_URL=https://rhos-d.infra.prod.upshift.rdu2.redhat.com:13808/v1/AUTH_95e858620fb34bcc9162d9f52367a560/osbuildci-artifacts
+sudo tee /etc/yum.repos.d/osbuild-mock.repo > /dev/null << EOF
+[osbuild-mock]
+name=osbuild mock ${BUILD_TAG} ${ID}${VERSION_ID//./}
+baseurl=${REPO_BASE_URL}/jenkins-osbuild-osbuild-composer-PR-651-26/${ID}${VERSION_ID//./}
+enabled=1
+gpgcheck=0
+# Default dnf repo priority is 99. Lower number means higher priority.
+priority=5
+EOF
+
+# Verify that the repository we added is working properly.
+dnf list all | grep osbuild-mock
+
 # Create temporary directories for Ansible.
 sudo mkdir -vp /opt/ansible_{local,remote}
 sudo chmod -R 777 /opt/ansible_{local,remote}
